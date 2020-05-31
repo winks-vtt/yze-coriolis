665563b98b09c5b23ed91808eda78b1b05796871
Alex Okafor <okafora@gmail.com>
2020-05-31 01:45:27 -0400

    added ability to roll items

--- module/item/item.js
+++ module/item/item.js
@@ -14,4 +14,21 @@ export class yzecoriolisItem extends Item {
     const actorData = this.actor ? this.actor.data : {};
     const data = itemData.data;
   }
+
+  async roll() {
+    // Basic template rendering data
+    const token = this.actor.token;
+    const item = this.data;
+    const actorData = this.actor ? this.actor.data.data : {};
+    const itemData = item.data;
+
+    // Define the roll formula.
+    let roll = new Roll('d20+@abilities.str.mod', actorData);
+    let label = `Rolling ${item.name}`;
+    // Roll and send to chat.
+    roll.roll().toMessage({
+      speaker: ChatMessage.getSpeaker({ actor: this.actor }),
+      flavor: label
+    });
+  }
 }
