75c3569ab5846e0d4264e66f50f619944effdd3b
Alex Okafor <okafora@gmail.com>
2020-06-28 19:04:55 -0400

    fix template path
    #21

--- module/templates.js
+++ module/templates.js
@@ -3,7 +3,7 @@ export const preloadHandlerbarsTemplates = async function () {
     const templatePaths = [
 
         // chat templates
-        "systems/yzecoriolis/templates/roll.html",
+        "systems/yzecoriolis/templates/sidebar/roll.html",
         "systems/yzecoriolis/templates/actor/parts/actor-bio.html",
         "systems/yzecoriolis/templates/actor/parts/actor-talents.html",
         "systems/yzecoriolis/templates/actor/parts/actor-stats.html",
