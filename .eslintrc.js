07fab3f0972d6ceebffa9407ff53533d1bd19da7
Alex Okafor <okafora@gmail.com>
2021-01-11 23:54:40 -0500

    added foundry eslint settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ["eslint:recommended", "prettier"],
+  extends: ["eslint:recommended", "plugin:foundry-vtt/recommended", "prettier"],
   parserOptions: {
     ecmaVersion: 12,
     sourceType: "module",
